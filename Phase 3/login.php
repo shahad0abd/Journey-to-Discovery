c805c7249b9b18fd707eb2aa5229645255fb449a
Joud <joudnaif99@gmail.com>
2024-11-25 09:01:51 +0300

    Update login.php

--- Phase 3/login.php
+++ Phase 3/login.php
@@ -52,7 +52,9 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 </head>
 <body>
     <header>
-        <img src="images/Web2-logo.svg" alt="Logo" class="logo">
+        <a href="http://JourneyToDiscovery.infinityfreeapp.com" target="_blank">
+            <img src="images/Web2-logo.svg" alt="Logo" class="logo">
+        </a>
     </header>
     <main>
         <h1>Login</h1>
